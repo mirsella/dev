451e15b34f6bd43af92639f2917732ba39f9698b
mirsella <mirsella@protonmail.com>
2020-11-15 20:33:55 +0100

    gacp dim. 15 nov. 2020 20:33:55 CET

---
+++ js/tampermonkey-ditchqwerty.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         ditchqwerty settings
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  apply settings
+// @author       You
+// @match        https://www.ditchqwerty.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+// one to left
+changeLayout(layoutList[currentLayoutIndex-1].innerHTML);
+currentLayoutIndex--;
+current = document.querySelector(".js-"+currentLayout);
+
+// to right
+changeLayout(layoutList[currentLayoutIndex+1].innerHTML);
+currentLayoutIndex++;
+current = document.querySelector(".js-"+currentLayout);
+
+// custom settigs
+wordLimitModeInput.value = 30
+scoreMax = 30
+
+// reload
+reset()
+
+
+})();
